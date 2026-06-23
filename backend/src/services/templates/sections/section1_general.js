7b459df5d85dfd212557685881fb7f5b80577b03
Nopphadol Nuntamool <nopphadol_n@payap.ac.th>
2026-06-23 10:39:13 +0700

    Fix section1 2 tqf3 pdf

--- backend/src/services/templates/sections/section1_general.js
+++ backend/src/services/templates/sections/section1_general.js
@@ -2,35 +2,35 @@ const renderSection1 = (data) => {
   const c = data.course || {};
   const { instructors, guestTeachers, user_id, owner_id } = data;
   return `
-    <div style="border: 1px solid #000000; padding: 3px; margin: 20px 0;">
-      <div style="border: 4px solid #000000; padding: 10px 0; text-align: center;">
+    <div style="border: 2px solid #000000; padding: 3px; margin: 20px 0;">
+      <div style="border: 5px solid #000000; padding: 10px 0; text-align: center;">
         <h1 style="margin: 0; font-size: 26px;">รายละเอียดของรายวิชา</h1>
       </div>
     </div>
-    <p><b>ชื่อสถาบันอุดมศึกษา</b>
+    <p><strong>ชื่อสถาบันอุดมศึกษา</strong>
     <span style="margin-left:10px;">มหาวิทยาลัยพายัพ</span></p>
-    <p><b>วิทยาเขต/คณะ/ภาควิชา/สาขาวิชา</b>
+    <p><strong>วิทยาเขต/คณะ/ภาควิชา/สาขาวิชา</strong>
     <span style="margin-left:10px;">คณะเภสัชศาสตร์</span></p>
     
-    <div style="border: 1px solid #000000; padding: 3px; margin: 20px 0;">
-      <div style="border: 4px solid #000000; padding: 10px 0; text-align: center;">
+    <div style="border: 2px solid #000000; padding: 3px; margin: 20px 0;">
+      <div style="border: 5px solid #000000; padding: 10px 0; text-align: center;">
         <h2 style="margin: 0; font-size: 24px;">หมวดที่ 1 ข้อมูลทั่วไป</h2>
       </div>
     </div>
-    <p><b>1. รหัสและชื่อรายวิชา</b></p>
+    <p><strong>1. รหัสและชื่อรายวิชา</strong></p>
         <p style="margin-left: 80px;">${c.code_th || '-'} ${c.name_th || '-'}</p>
         <p style="margin-left: 80px;">${c.code_en || '-'} ${c.name_en || '-'}</p>
 
-    <p><b>2. จำนวนหน่วยกิต</b></p> 
+    <p><strong>2. จำนวนหน่วยกิต</strong></p> 
         <p style="margin-left: 80px;">${c.credits || '-'} หน่วยกิต (${c.credit_format || '-'})</p>
 
-    <p><b>3. หลักสูตรและประเภท:</b></p>
+    <p><strong>3. หลักสูตรและประเภทของรายวิชา</strong></p>
         <p style="margin-left: 80px;">ชื่อหลักสูตร
         <span style="margin-left:40px;">เภสัชศาสตรบัณฑิต สาขาวิชาการบริบาลทางเภสัชกรรม (หลักสูตรปรับปรุง พ.ศ.2568)</span></p>
         <p style="margin-left: 80px;">ประเภทของรายวิชา
         <span style="margin-left:20px;">${c.course_type || '-'}</span></p>
 
-    <p><b>4. อาจารย์ผู้รับผิดชอบรายวิชาและอาจารย์ผู้สอน</b></p>
+    <p><strong>4. อาจารย์ผู้รับผิดชอบรายวิชาและอาจารย์ผู้สอน</strong></p>
         <p style="margin-left: 80px;">อาจารย์ผู้รับผิดชอบรายวิชา
         <span style="margin-left:5px;">
     ${instructors.find(i => i.id === c.owner_id)?.name_th || '-'}
@@ -47,17 +47,17 @@ const renderSection1 = (data) => {
     : ''
     }
             <p style="margin-left: 120px;">ภาคปฏิบัติ</p>
-    <p><b>5. ภาคการศึกษา / ชั้นปีที่เรียน</b> 
+    <p><strong>5. ภาคการศึกษา / ชั้นปีที่เรียน</strong> 
         <p style="margin-left: 80px;">ภาคการศึกษาที่ ${c.semester || '-'} ปีการศึกษา ${c.year || '-'}</p>
         <p style="margin-left: 80px;">ชั้นปีที่........</p>
 
-    <p><b>6. รายวิชาที่ต้องเรียนมาก่อน (Pre-requisite) (ถ้ามี)</b></p> 
+    <p><strong>6. รายวิชาที่ต้องเรียนมาก่อน (Pre-requisite) (ถ้ามี)</strong></p> 
         <p style="margin-left: 80px;">${c.prerequisite || '-'}</p>
-    <p><b>7. รายวิชาที่ต้องเรียนพร้อมกัน (Co-requisites) (ถ้ามี)</b></p>
+    <p><strong>7. รายวิชาที่ต้องเรียนพร้อมกัน (Co-requisites) (ถ้ามี)</strong></p>
         <p style="margin-left: 80px;"> - </p>
-    <p><b>8. สถานที่เรียน</b></p>
+    <p><strong>8. สถานที่เรียน</strong></p>
         <p style="margin-left: 80px;">คณะเภสัชศาสตร์ มหาวิทยาลัยพายัพ</p>
-    <p><b>9. วันที่จัดทำหรือปรับปรุงรายละเอียดของรายวิชาครั้งล่าสุด</b></p>
+    <p><strong>9. วันที่จัดทำหรือปรับปรุงรายละเอียดของรายวิชาครั้งล่าสุด</strong></p>
         <p style="margin-left: 80px;"> - </p>
   `;
 
--- backend/src/services/templates/sections/section2_clo.js
+++ backend/src/services/templates/sections/section2_clo.js
@@ -3,12 +3,15 @@ const renderSection2 = (data) => {
 
   return `
   <div class="section">
-    <h2>หมวดที่ 2 CLO</h2>
-
+    <div style="border: 2px solid #000000; padding: 3px; margin: 20px 0;">
+      <div style="border: 5px solid #000000; padding: 10px 0; text-align: center;">
+        <h2 style="margin: 0; font-size: 24px;">หมวดที่ 2 ผลลัพธ์การเรียนรู้ระดับรายวิชา</h2>
+      </div>
+    </div>
+<p>เมื่อสิ้นสุดการเรียนการสอนแล้ว นักศึกษาที่สำเร็จการศึกษาในรายวิชา สามารถ (CLOs)<p>
     ${clos.map((c,i)=>`
-      <p><strong>${i+1}. ${c.code}</strong> ${c.description}</p>
+      <p style="margin-left: 80px;">${i+1}. ${c.description}</p>
     `).join('')}
-
   </div>
   `;
 };

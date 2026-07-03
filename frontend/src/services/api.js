299be90e85c7470531d63b86fb4f9f5aa02b19af
Nopphadol Nuntamool <nopphadol_n@payap.ac.th>
2026-07-03 18:53:27 +0700

    Fix clo page

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,12 +7,27 @@ const api = axios.create({
 // แนบ token อัตโนมัติ
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
-
   return config;
 });
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (
+      error.response &&
+      (
+        error.response.status === 401 ||
+        error.response.status === 403
+      )
+    ) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
